9f727ab18db6816fc77eafa590bebbc149b2a236
efrat-bar-sheshet <Hand4d6@mbjcomp.org.il>
2025-06-13 17:20:19 +0300

    עדכון עיצוב או לוגיקה בקובץ Login.jsx

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -5,17 +5,17 @@ import APIRequests from '../../services/APIRequests';
 // import axios from 'axios';
 // import "../Auth/Login.css"
 
-// export function Login() {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [error, setError] = useState('');
-//   const { setUser } = useUser();
-//   const navigate = useNavigate();
+export function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { setUser } = useUser();
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await APIRequests.getRequest(`users?username=${username}&password=${password}`)
+      const response = await APIRequests.getRequest(users?username=${username}&password=${password})
       const user = response.username;
       const token = response.token; 
       setUser(user);
@@ -27,36 +27,36 @@ import APIRequests from '../../services/APIRequests';
     }
   };
 
-//   return (
-//     <div>
-//       <h2>Login</h2>
-//       <form onSubmit={handleLogin}>
-//         <div>
-//           <label> User Name:</label>
-//           <input
-//             type="text"
-//             value={username}
-//             onChange={(e) => setUsername(e.target.value)}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Password:</label>
-//           <input
-//             type="password"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//             required
-//           />
-//         </div>
-//         <div>
-//           {error && <p style={{ color: 'red' }}>{error}</p>}
-//         </div>
-//         <button type="submit">Connect</button>
-//       </form>
-//       <Link to="/register">Don't have an account? Register here</Link>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label> User Name:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+        </div>
+        <button type="submit">Connect</button>
+      </form>
+      <Link to="/register">Don't have an account? Register here</Link>
+    </div>
+  );
+}
 
-// export default Login;
+export default Login;
