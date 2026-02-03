746e8a12c543ac9adc8982f07bd6dfacf9dab867
Ayala-Barsheshet <Hand4d5@mbjcomp.org.il>
2026-02-03 17:19:43 +0200

    Restore and secure database configuration

---
+++ server/DB/mysql.js
@@ -0,0 +1,18 @@
+import mysql from 'mysql2';
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME, 
+});
+
+con.connect((err) => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + con.threadId);
+});
+
+export default con;
